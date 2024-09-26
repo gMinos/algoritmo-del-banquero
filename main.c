04b797aa0067fe995d0a95818bed0b71352ce103
gMinos <gjosebelarmino@gmail.com>
2024-09-26 00:36:11 +0000

    mejorando el tipado

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void llenar(int **c, int **a, int *disponible, int fila, int columna) {
+void llenar(int **c, int **a, int *disponible, int8_t fila, int8_t columna) {
   FILE *numeros = fopen("prueba.txt", "r");
   if (numeros != NULL) {
-    for (int i = 0; i < fila; i++) {
+    for (int8_t i = 0; i < fila; i++) {
       fscanf(numeros, "%d,%d,%d", &c[i][0], &c[i][1], &c[i][2]);
     }
-    for (int i = 0; i < fila; i++) {
+    for (int8_t i = 0; i < fila; i++) {
       fscanf(numeros, "%d,%d,%d", &a[i][0], &a[i][1], &a[i][2]);
     }
     fscanf(numeros, "%d,%d,%d", &disponible[0], &disponible[1], &disponible[2]);
@@ -15,25 +16,25 @@ void llenar(int **c, int **a, int *disponible, int fila, int columna) {
   }
 }
 
-int **crearMatrizDinamica(int fila, int columna) {
+int **crearMatrizDinamica(int8_t fila, int8_t columna) {
   int **matriz = (int **)malloc(fila * sizeof(int *));
-  for (int i = 0; i < fila; i++) {
+  for (int8_t i = 0; i < fila; i++) {
     matriz[i] = (int *)malloc(columna * sizeof(int));
   }
   return matriz;
 }
 
-void restar(int **c, int **a, int **ca, int fila, int columna) {
-  for (int i = 0; i < fila; i++) {
-    for (int j = 0; j < columna; j++) {
+void restar(int **c, int **a, int **ca, int8_t fila, int8_t columna) {
+  for (int8_t i = 0; i < fila; i++) {
+    for (int8_t j = 0; j < columna; j++) {
       ca[i][j] = c[i][j] - a[i][j];
     }
   }
 }
 
-void imprimir(int **matriz, int fila, int columna, char *caracter) {
-  for (int i = 0; i < fila; i++) {
-    for (int j = 0; j < columna; j++) {
+void imprimir(int **matriz, int8_t fila, int8_t columna, char *caracter) {
+  for (int8_t i = 0; i < fila; i++) {
+    for (int8_t j = 0; j < columna; j++) {
       printf("%d ", matriz[i][j]);
     }
     printf("\n");
@@ -44,18 +45,18 @@ void imprimir(int **matriz, int fila, int columna, char *caracter) {
 
 int *arregloDinamico(int tamano) { return (int *)malloc(tamano * sizeof(int)); }
 
-void imprimirArreglo(int *arreglo, int tamano) {
-  for (int i = 0; i < tamano; i++) {
+void imprimirArreglo(int *arreglo, int8_t tamano) {
+  for (int8_t i = 0; i < tamano; i++) {
     printf("%d ", arreglo[i]);
   }
   printf("disponible");
   printf("\n-----------\n");
 }
 
-int sumaFilaMatrizCA(int **ca, int fila) {
-  int sumar = 0;
-  for (int i = (fila - 1); i < fila; i++) {
-    for (int j = 0; j < 3; j++) {
+int8_t sumaFilaMatrizCA(int **ca, int fila) {
+  int8_t sumar = 0;
+  for (int8_t i = (fila - 1); i < fila; i++) {
+    for (int8_t j = 0; j < 3; j++) {
       sumar += ca[i][j];
     }
   }
@@ -63,11 +64,11 @@ int sumaFilaMatrizCA(int **ca, int fila) {
   return sumar;
 }
 
-int evaluarFilas(int **ca, int *disponible, int aux) {
-  int contador = 0;
-  for (int i = (aux - 1); i < aux; i++) {
-    for (int j = 0; j < 3; j++) {
-      if (ca[i][j] <= disponible[j] && sumaFilaMatrizCA(ca, aux) != 0) {
+int evaluarFilas(int **ca, int *disponible, int8_t fila) {
+  int8_t contador = 0;
+  for (int8_t i = (fila - 1); i < fila; i++) {
+    for (int8_t j = 0; j < 3; j++) {
+      if (ca[i][j] <= disponible[j] && sumaFilaMatrizCA(ca, fila) != 0) {
         contador++;
       }
     }
@@ -76,40 +77,40 @@ int evaluarFilas(int **ca, int *disponible, int aux) {
   return contador;
 }
 
-void sumarFilaMatrizA(int **a, int **ca, int fila) {
-  for (int i = 0; i < 3; i++) {
+void sumarFilaMatrizA(int **a, int **ca, int8_t fila) {
+  for (int8_t i = 0; i < 3; i++) {
     a[fila][i] += ca[fila][i];
   }
 }
 
-void restarFilaMatrizCA(int **ca, int posicion) {
-  for (int i = 0; i < 3; i++) {
+void restarFilaMatrizCA(int **ca, int8_t posicion) {
+  for (int8_t i = 0; i < 3; i++) {
     ca[posicion][i] -= ca[posicion][i];
   }
 }
 
-void restarRecursosDisponibles(int **ca, int *disponible, int fila) {
-  for (int i = 0; i < 3; i++) {
+void restarRecursosDisponibles(int **ca, int *disponible, int8_t fila) {
+  for (int8_t i = 0; i < 3; i++) {
     disponible[i] -= ca[fila][i];
   }
 }
 
-void sumarRecursosDisponibles(int **a, int *disponible, int fila) {
-  for (int i = 0; i < 3; i++) {
+void sumarRecursosDisponibles(int **a, int *disponible, int8_t fila) {
+  for (int8_t i = 0; i < 3; i++) {
     disponible[i] += a[fila][i];
   }
 }
 
-void liberarMemoriaMatrizA(int **c, int **a, int fila) {
-  for (int i = 0; i < 3; i++) {
+void liberarMemoriaMatrizA(int **c, int **a, int8_t fila) {
+  for (int8_t i = 0; i < 3; i++) {
     if (c[fila][i] == a[fila][i]) {
       a[fila][i] = 0;
     }
   }
 }
 
-void procedimientoMatrices(int **c, int **a, int **ca, int *disponible, int fila, int columna) {
-  int resultado = 0;
+void procedimientoMatrices(int **c, int **a, int **ca, int *disponible, int8_t fila, int8_t columna) {
+  int8_t resultado = 0;
   for (int k = 1; k <= fila; k++) {
     resultado = evaluarFilas(ca, disponible, k);
     if (resultado == 3) {
@@ -127,18 +128,18 @@ void procedimientoMatrices(int **c, int **a, int **ca, int *disponible, int fila
   }
 }
 
-int validarEstadoMatrizCA(int **ca, int fila, int columna) {
-  int suma = 0;
-  for (int i = 0; i < fila; i++) {
-    for (int j = 0; j < columna; j++) {
+int8_t validarEstadoMatrizCA(int **ca, int8_t fila, int8_t columna) {
+  int8_t suma = 0;
+  for (int8_t i = 0; i < fila; i++) {
+    for (int8_t j = 0; j < columna; j++) {
       suma += ca[i][j];
     }
   }
   return suma;
 }
 
-void liberarMemoria(int **ca, int **a, int **c, int fila) {
-  for (int i = 0; i < fila; i++) {
+void liberarMemoria(int **ca, int **a, int **c, int8_t fila) {
+  for (int8_t i = 0; i < fila; i++) {
     free(c[i]);
     free(a[i]);
     free(ca[i]);
@@ -150,7 +151,7 @@ void liberarMemoria(int **ca, int **a, int **c, int fila) {
 }
 
 int main() {
-  int fila = 4, columna = 3;
+  int8_t fila = 4, columna = 3;
   int **c = crearMatrizDinamica(fila, columna);
   int **a = crearMatrizDinamica(fila, columna);
   int **ca = crearMatrizDinamica(fila, columna);
@@ -172,5 +173,5 @@ int main() {
 
   liberarMemoria(ca, a, c, fila);
 
-    return 0;
+  return 0;
 }
